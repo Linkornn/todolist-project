96b02486509653e3f75fa405937e9dfe83e3678d
Linkornn <linkornn2003@gmail.com>
2023-10-10 23:26:38 +0700

    back to og

--- front/public/scripts/script.js
+++ front/public/scripts/script.js
@@ -64,4 +64,8 @@ function updateUIWithTask(taskData, taskId) {
     cell3.innerHTML = taskData.time.trim() === "" ? "No Due" : taskData.time;
     cell4.innerHTML = '<button onclick="deleteTask(this)">Delete</button>';
 }
+function deleteTask(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("myTable").deleteRow(i);
 
+  }
